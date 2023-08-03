af6f5847b69f9f7ed48a08e34cf62aa9af6d9686
Joseph Pecoraro <jpecoraro342@yahoo.com>
2023-08-03 09:01:25 -0400

    Initial Commit

---
+++ VaroMobileEngineeringProject/VaroMobileEngineeringProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VaroMobileEngineeringProject
+//
+//  Created by Joseph Pecoraro on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
